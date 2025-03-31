e0364ec39b10cae553895da9f4950b3e7579a709
Erik Ø. Sørensen <erik.sorensen@nhh.no>
2025-03-31 19:24:36 +0200

    Reorganizing graph save.

---
+++ zipping_graphs_and_tables.sh
@@ -0,0 +1,7 @@
+zip mmzame_graphs.zip graphs/Figures_1.pdf \
+  graphs/Figures_2.pdf \
+  graphs/aggregate_choices.pdf \
+  graphs/logprice_scatters.pdf \
+  graphs/empirical_cceis.pdf \
+  graphs/empirical_cceis_and_Bronars.pdf \
+  graphs/prop3_permutations.pdf 
